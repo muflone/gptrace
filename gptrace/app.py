f0d51ca97ce492a0b48eb671c668419bb25c70c6
Muflone <webreg@muflone.com>
2021-07-11 12:15:15 +0200

    Explicitly require Gtk 3

--- gptrace/app.py
+++ gptrace/app.py
@@ -18,6 +18,9 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
 
+import gi
+gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gio
 from gi.repository import Gtk
 
