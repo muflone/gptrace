4aaaa69a814576b729e03b8a6edb3837d8186c90
Muflone <webreg@muflone.com>
2022-05-22 12:14:30 +0200

    Bumped version number

--- gptrace/constants.py
+++ gptrace/constants.py
@@ -25,7 +25,7 @@ from xdg import BaseDirectory
 
 # Application constants
 APP_NAME = 'gpTrace'
-APP_VERSION = '0.5.0'
+APP_VERSION = '0.6.0'
 APP_DESCRIPTION = 'Trace the activities of an external application.'
 APP_ID = 'gptrace.muflone.com'
 APP_URL = 'https://www.muflone.com/gptrace/'
